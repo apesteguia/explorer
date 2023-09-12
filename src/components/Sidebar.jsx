75543b1c76665d2963427f92ce23d80638ffb6d4
apesteguia <mikeltxomikeltxo@gmail.com>
2023-09-12 16:26:44 +0200

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,13 @@
+import FolderIcon from "../img/folder.svg";
+import { IconHome } from "@tabler/icons-solidjs";
+
 export default function Sidebar() {
   return (
-    <div className="border border-slate-700 fixed min-w-[25%] top-[30px] h-[calc(100%-30px)] flex flex-col items-left"></div>
+    <div className="border border-slate-700 fixed min-w-[25%] top-[30px] h-[calc(100%-30px)] flex flex-col items-left">
+      <button className="flex gap-3 w-full items-center  h-[40px] hover:bg-slate-700">
+        <img src={FolderIcon} className="ml-3 w-[30px] h-[30px]" alt="" />
+        <p>Home</p>
+      </button>
+    </div>
   );
 }
